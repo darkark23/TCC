3977876757b81221d985e5f06c696936d9d4ef3f
daniel <danielmedeiros1988@gmail.com>
2020-02-04 22:04:57 -0300

    Adicionando Repositorios para paginas de ledor

---
+++ src/main/java/br/net/iesb/controller/LivroController.java
@@ -0,0 +1,48 @@
+package br.net.iesb.controller;
+
+import br.net.iesb.dto.AudioLivroDetalheDTO;
+import br.net.iesb.dto.AudioLivroSelecaoDTO;
+import br.net.iesb.dto.LivroDTO;
+import br.net.iesb.dto.LivroNovoDTO;
+import br.net.iesb.service.transacional.AudioLivroService;
+import br.net.iesb.service.transacional.LivroService;
+import br.net.iesb.view.AudioLivroView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/livro")
+public class LivroController {
+
+    final LivroService livroService;
+
+    @Autowired
+    public LivroController(LivroService livroService) {
+        this.livroService = livroService;
+    }
+
+    @ResponseBody
+    @GetMapping(path = "/findById/{id}")
+    public LivroDTO findById(@PathVariable ("id") Long id) {
+        return livroService.findById(id);
+    }
+
+    @ResponseBody
+    @GetMapping(path = "/findAll")
+    public List<LivroDTO> findAll() {
+        return livroService.findAll();
+    }
+
+    @ResponseBody
+    @GetMapping(path = "/save")
+    public Integer save(LivroNovoDTO livroNovoDTO) {
+        return livroService.saveLivroNovo(livroNovoDTO);
+    }
+
+}
