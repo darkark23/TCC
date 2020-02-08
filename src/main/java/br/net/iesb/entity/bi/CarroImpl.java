fa22eaf19e198f42dcd34982cd5cc40690611239
Daniel <danielnogueira.co@gmail.com>
2020-02-08 19:51:09 -0300

    Itens de menu e logica de login para perfil de Ledor adicionado

---
+++ src/main/java/br/net/iesb/entity/bi/CarroImpl.java
@@ -0,0 +1,12 @@
+package br.net.iesb.entity.bi;
+
+public class CarroImpl implements VeiculoImpl,VeiculoImpl2 {
+
+	@Override
+	public void adicionarVeiculo(String veiculo) {
+		this.adicionarVeiculo("dsdsd");
+	}
+
+
+}
+
---
+++ src/main/java/br/net/iesb/entity/bi/VeiculoImpl.java
@@ -0,0 +1,14 @@
+package br.net.iesb.entity.bi;
+
+public interface VeiculoImpl {
+
+	public String dddd = null;
+
+	public void adicionarVeiculo(String veiculo);
+	public static void adicionarVeiculo(Integer veiculo){
+		System.out.println("teste0sdasd2");
+	};
+
+
+}
+
